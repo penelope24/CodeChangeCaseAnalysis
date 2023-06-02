312922bdc2ff38e82463944b0830a8173bb2f948
fy <rachmanino@yahoo.com>
2023-06-02 16:18:06 +0800

    update

---
+++ src/main/java/change/case1.java
@@ -0,0 +1,26 @@
+package change;
+
+import java.util.Queue;
+
+public class case1 {
+
+    public void func(Queue<Integer> marksQueue) {
+        int Pass, Fail, Count;
+        Pass = 0 ;
+        Fail = 0 ;
+        Count = 0 ;
+        while (!marksQueue.isEmpty()) {
+            int Marks = marksQueue.poll();
+            if (Marks >= 40) {
+                Pass = Pass + 1;
+            }
+            if (Marks < 40) {
+                Fail = Fail + 1;
+            }
+            Count = Count + 1;
+        }
+        System.out.println(Pass);
+        System.out.println(Fail);
+        System.out.println(Count);
+    }
+}
